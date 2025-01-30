77c4e3a2a997d3277de3cc782e6029b501757cd8
anuj-dhamne <anujdhamne10@gmail.com>
2025-01-30 10:04:02 +0530

    addExpenses style  Done

--- expenseManager/src/components/AddExpenseForm/AddExpenseForm.jsx
+++ expenseManager/src/components/AddExpenseForm/AddExpenseForm.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
 import LinkButton from '../LinkButton/LinkButton'
+import "./form.css"
 
 function AddExpenseForm() {
-    const [expense,setExpense]=useState({})
-  return (
-    <>
-        <form action="">
-            <label htmlFor="title">Title : </label>
-            <input type="text" placeholder='Title' id='title'/>
+  
+  const [data,setData]=useState();
+  const onSubmitHandler=()=>{
 
+  }
+  return (
+    <div className="main">
+        <form action="" onSubmit={onSubmitHandler}>
+           <div className="subDiv">
+           <label htmlFor="title">Title : </label>
+           <input type="text" placeholder='Title' id='title' />
+           </div>
+
+
+            <div className="subDiv">
             <label htmlFor="category">Category : </label>
-            <select name="category" id="category">
+            <select name="category" id="category" >
                 <option value="Food">Food & Dining</option>
                 <option value="Travel">Travel</option>
                 <option value="Entertainment">Entertainment</option>
@@ -18,20 +27,34 @@ function AddExpenseForm() {
                 <option value="Utilites">Utilites</option>
                 <option value="Miscellaneous">Miscellaneous</option>
             </select>
+            </div>
 
+            <div className="subDiv">
             <label htmlFor="amount">Amount : </label>
             <input type="number" name="amount" id="amount" />
+            </div>
 
-            <label htmlFor="date">Date</label>
-            <input type="date" name="date" id="date" />
+           <div className="subDiv">
+           <label htmlFor="date">Date</label>
+           <input type="date" name="date" id="date" />
+           </div>
 
+           <div className="subDiv">
+           <label htmlFor="date">Bill</label>
+           <input type="file" name="bill" id="bill" />
+           </div>
+
+            <div className="subDiv">
             <label htmlFor="description">Description</label>
-          <textarea name="description" id="description"></textarea>
+            <textarea name="description" id="description"></textarea>
+            </div>
         </form>
-        <LinkButton children="Cancel" url=""/>
-        <LinkButton children="Add" url="add-expenses"/>
-           
-    </>
+
+       <div className=" btnBox" >
+       <LinkButton children="Cancel" url="#" className="btn" />
+       <LinkButton children="Add" url="add-expenses" className="btn"/>
+       </div>
+    </div>
   )
 }
 
