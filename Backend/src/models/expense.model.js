4d3e38d52b7087ecb7f24ab096643e8c47323818
anuj-dhamne <anujdhamne10@gmail.com>
2025-02-01 11:37:04 +0530

    Done databse Connection

---
+++ Backend/src/models/expense.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema(
+    {
+        title:{
+            type:String,
+            lowercase:true,
+            required:true,
+            trim:true,
+        },
+        category:{
+            type:String,
+            required:true,
+            lowercase:true,
+        },
+        amount:{
+            type:Number,
+            required:true,
+        },
+        date:{
+            type:Date,
+            required:true
+        },
+        bill_photo:{
+            type:String,
+        },
+        description:{
+            type:String
+        }
+    }
+    , { timestamps: true });
+
+export const Expense = new mongoose.model("Expense", expenseSchema);
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose"
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true,
+        index:true
+    },
+    fullname:{
+        type:String,
+        required:true,
+        lowercase:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true
+    },
+    avator:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is Required"],
+    },
+    BudgetAmount:{
+        type:Number,
+        required:true,
+    },
+    expenseAmount:{
+        type:Number,
+    },
+    expensesList:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Expense"
+        }
+    ],
+    refreshToken:{
+        type:String,
+    }
+
+},{timestamps:true});
+
+export const User=new mongoose.model("User",userSchema);
